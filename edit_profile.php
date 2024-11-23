cf1ff80f077102b551eaab2852bfcce79c7da4cf
Hasan <202206958@stu.uob.edo.bh>
2024-11-23 21:48:59 +0300

    Fetching Current User Data

--- edit_profile.php
+++ edit_profile.php
@@ -1,3 +1,8 @@
 <?php
 session_start();
 require 'db.php';
+
+$userId = $_SESSION['user_id'];
+$stmt = $pdo->prepare("SELECT * FROM users WHERE id = ?");
+$stmt->execute([$userId]);
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
