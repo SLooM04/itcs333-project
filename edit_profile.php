bf9d44bda170afdef590fb0e6b541c4680c80157
Hasan <202206958@stu.uob.edo.bh>
2024-11-23 21:51:10 +0300

    Handling Form if  Submission happend

--- edit_profile.php
+++ edit_profile.php
@@ -6,3 +6,10 @@ $userId = $_SESSION['user_id'];
 $stmt = $pdo->prepare("SELECT * FROM users WHERE id = ?");
 $stmt->execute([$userId]);
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = trim($_POST['name']);
+    $bio = trim($_POST['bio']);
+    $email = trim($_POST['email']);
+    $password = $_POST['password'];
+    $profilePicture = $user['profile_picture'];}
