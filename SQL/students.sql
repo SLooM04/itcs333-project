5cf531bf73803c3b3d0dd285d08b60b8fc4317ef
Hasan <mrzizt04@gmail.com>
2024-12-09 03:45:12 +0300

    delete credit

--- SQL/students.sql
+++ SQL/students.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `students` (
-  `student_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(50) NOT NULL,
   `last_name` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
@@ -11,6 +11,5 @@ CREATE TABLE `students` (
   `profile_picture` varchar(255) DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `booking_credit` int(11) NOT NULL DEFAULT 10 COMMENT 'Number of credits for booking rooms',
   PRIMARY KEY (`student_id`)
 );
